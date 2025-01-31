e815caa32bf4b3b54fa1b8860dbdd2db6de7d2fc
soul redouane <101919227+SOULREDOUANE@users.noreply.github.com>
2025-01-31 10:57:19 +0100

    Update database.php

--- src/database.php
+++ src/database.php
@@ -4,13 +4,13 @@
 $db_host = getenv('DB_HOST');
 $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
-$password_file_path = getenv('PASSWORD_FILE_PATH');
+$db_password = getenv('DB_PASSWORD');
 
 // Read the password file path from an environment variable
 $password_file_path = getenv('PASSWORD_FILE_PATH');
 
 // Read the password from the file
-$db_pass = trim(file_get_contents($password_file_path));
+$db_pass = getenv('db_password')
 
 // Create a new PDO instance
 $db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
